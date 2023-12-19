9c0afe988579a64568e8314e6a4405d0190af09a
mfhooks88 <mfhooks88@gmail.com>
2023-12-18 21:07:43 -0500

    rough draft all files

---
+++ script.js
@@ -0,0 +1,31 @@
+var quizQuestions = [
+    {
+        question: "The condition in an if/else statement is enclosed with?",
+        options: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        correctAnswer: "parenthesis"
+    },
+    {
+        question: "Commonly used data types do NOT include?",
+        options: ["strings", "booleans", "alerts", "numbers"],
+        correctAnswer: "alerts"
+    },
+    {
+        question: "Arrays in JavaScipt can be used to store?",
+        options: ["numbers and strings", "other arrays", "booleans", "all the above"],
+        correctAnswer: "all the above"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is?",
+        options: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        correctAnswer: "console.log"
+    },
+    {
+        question: "In HTML, the H stands for what?",
+        options: ["hyper", "hippo", "hypo", "hello"],
+        correctAnswer: "hyper"
+    },
+]
+
+function startQuiz (){
+    
+}
